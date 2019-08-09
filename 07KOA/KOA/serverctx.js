67b403eadbec5bd8f669c8e243815977b65a782d
xiaoyeeee <xiaoyefordevelopment@outlook.com>
2019-08-09 14:27:54 +0800

    ~

--- 07KOA/KOA/serverctx.js
+++ 07KOA/KOA/serverctx.js
@@ -2,9 +2,16 @@ const koa = require("koa");
 
 let server = new koa();
 server.listen(8080);
-server.context.a = 1;
+//server.context.a = 1;
 
-server.use(async(ctx, next) => {
-    ctx.body = ctx.a;
-    await next();
+server.use(async (ctx, next) => {
+    //ctx.body = ctx.a;
+    // await next();
+    console.log("header:" + ctx.header);
+    console.log("method:" + ctx.method);
+    console.log("url:" + ctx.url);
+    console.log("href:" + ctx.href);
+    console.log("path:" + ctx.path);
+    console.log("querystring:" + ctx.querystring);
+    console.log("host:" + ctx.host);
 });
